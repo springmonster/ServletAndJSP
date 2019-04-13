dc0de421759d0cfb160be4aaa774229638fed6d4
springmonster <charleskuanghc@163.com>
2019-04-13 22:25:10 +0800

    servlet context

---
+++ Chapter5/src/cc/openhome/Avatar.java
@@ -0,0 +1,70 @@
+package cc.openhome;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Avatar
+ */
+@WebServlet(name = "Avatar", urlPatterns = { "/avatar.view" }, initParams = {
+		@WebInitParam(name = "AVATAR_DIR", value = "/avatars") })
+public class Avatar extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String AVATAR_DIR = "";
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Avatar() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+		AVATAR_DIR = getInitParameter("AVATAR_DIR");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>头像显示</title>");
+		out.println("</head>");
+		out.println("<body>");
+		for (String avatar : getServletContext().getResourcePaths(AVATAR_DIR)) {
+			System.out.println("avatar is " + avatar);
+			avatar = avatar.replaceFirst("/", "");
+			out.println("<img src='" + avatar + "'>");
+		}
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
