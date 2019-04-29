809ae1d58ec737b1b45f1b5f136d71ec951be4b1
springmonster <charleskuanghc@163.com>
2019-04-29 20:43:41 +0800

    add el

---
+++ HeadFirst/WebContent/music.jsp
@@ -0,0 +1,15 @@
+<%@page import="com.example.jsp.Counter"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	Music is ${music[0]}
+	<br> Music is ${music[1]}
+	<br> Music is ${music}
+</body>
+</html>
---
+++ HeadFirst/src/com/example/el/ELServlet4.java
@@ -0,0 +1,46 @@
+package com.example.el;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ELServlet4
+ */
+@WebServlet("/ELServlet4")
+public class ELServlet4 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ELServlet4() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String[] musicList = { "1", "2", "3" };
+		request.setAttribute("music", musicList);
+		request.getRequestDispatcher("music.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
